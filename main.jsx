156b004ed5724fccce5697e2d672914ffa40a588
Sebiche100 <sebi.monterod@gmail.com>
2025-02-12 11:24:34 -0500

    Agregar frontend y conexión con backend

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/TransferenciaForm.css'; // Importa los estilos
+import TransferenciaForm from './components/TransferenciaForm';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TransferenciaForm />
+  </React.StrictMode>
+);
